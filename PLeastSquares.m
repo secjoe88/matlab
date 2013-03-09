bbd4325dd08c315c01531073d9d1c3adad8c4506
joeywillhite <joeycw328@gmail.com>
2013-03-09 14:19:22 -0800

    added ability to compute least square of function as well as datasets. changed input structure to user input for data/functions

--- PLeastSquares.m
+++ PLeastSquares.m
@@ -1,33 +1,68 @@
 % Joey Willhite
 % Description:
 %     A funciton to create polynomial approximations of degree n to arbitrary
-%     datasets using the Least Squares approximation method.
+%     datasets or to a function using the Least Squares approximation method.
 % Inputs:
-%     x_i: The x values of the given dataset
-%     y_i: The y values of the given dataset
-%     a_i: Symbolic variables corresponding to the coefficients of the
-%          approximating polynomial
+%     option: String representing whether the method is to approximate a collection
+%             of data (with option='data') or a function (with
+%             option='function')
 %     n: The degree of the desired polynomial approximation
+%     *Note*
+%       Function will ask for user input for data or function/interval
+%       depending on value of 'option' parameter.
 % Outputs:
 %     solutions: Struct containing the calculated coefficients of the
 %     polynomial approximation
-function solutions=PLeastSquares(x_i, y_i, a_i, n)
+function solutions=PLeastSquares(option, n)
+    a_i=sym('a', [1 n+1]);
     eqs=cell(n+1, 1);
-    for i=1:n+1
-        eqs{i}=buildEq(x_i,y_i,a_i,n,i);
+    if strcmp(option,'data')==1 
+        x_i=input('Input x values: ');
+        y_i=input('Input y values: ');
+        for i=1:n+1
+            eqs{i}=buildEq(option, x_i, y_i, a_i, n, i);
+        end
+    elseif strcmp(option, 'function')==1
+        syms x;
+        func=input('Input function (as symbolic function of x): ');
+        interval=zeros(1,2);
+        interval(1)=input('Beginning of interval: ');
+        interval(2)=input('End of interval: ');
+        for i=1:n+1
+            eqs{i}=buildEq(option, func, interval, a_i, n, i);
+        end
+        
     end
     solutions=solve([eqs{1:length(eqs)}]);
 end
 
-%Build normal equations
-function eq=buildEq(xs, ys, coeffs, degree, eq_no)
+%Function to build normal equations:
+%   This function builds the corresponding normal equations to be solved
+%   for the coefficients of the polynomial approximation. It uses a switch
+%   statement based on the 'option' parameter to determine whether it will
+%   build normal equations pursuant to an approximation of data, with arg1
+%   and arg2 being the x and y values respectively, or pursuant to an
+%   approximation of a function, with arg1 and arg2 being the function and
+%   interval of aproximation respectively.
+function eq=buildEq(option, arg1, arg2, coeffs, degree, eq_no)
     eq=0;
     j=1;
-    for i=(eq_no-1):degree+(eq_no-1)
-        eq=eq+(coeffs(j)*sigmaSingle(xs,i));
-        j=j+1;
+    switch option
+        case 'data'
+            for i=(eq_no-1):degree+(eq_no-1)
+                eq=eq+(coeffs(j)*sigmaSingle(arg1,i));
+                j=j+1;
+            end
+            eq=eq==sigmaDouble(arg1,arg2,eq_no-1);
+        case 'function'
+            syms x;
+            for i=(eq_no-1):degree+(eq_no-1)
+                eq=eq+(coeffs(j)*int(x^i, arg2(1), arg2(2)));
+                j=j+1;
+            end
+            eq=eq==int((x^(eq_no-1))*arg1,arg2(1),arg2(2));
     end
-    eq=eq==sigmaDouble(xs,ys,eq_no-1);
+    
 end
 
 %compute sigma function for the sum of x values to the degree specified
